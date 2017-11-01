dfa6b00d0b9424979ec3bf275cb3d891ff2b3bc7
aijunx <aijun198600@126.com>
2017-11-02 00:27:08 +0800

    新增一个模块

---
+++ ajotherlog/src/main/java/com/aijun/jitpack/ajotherlog/AJOtherLog.java
@@ -0,0 +1,13 @@
+package com.aijun.jitpack.ajotherlog;
+
+/**
+ * Created by Administrator on 2017/11/2.
+ */
+
+public class AJOtherLog {
+    public static String message = "另外一个模块的信息";
+
+    public static void log() {
+        System.out.println(message);
+    }
+}
