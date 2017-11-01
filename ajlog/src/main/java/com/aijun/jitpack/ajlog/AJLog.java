d6d3121f886c45ee2a547c12ca377fc37fb4c1e0
aijunx <aijun198600@126.com>
2017-11-01 23:37:24 +0800

    上传公共模块代码1.0.0

---
+++ ajlog/src/main/java/com/aijun/jitpack/ajlog/AJLog.java
@@ -0,0 +1,11 @@
+package com.aijun.jitpack.ajlog;
+
+/**
+ * Created by Administrator on 2017/11/1.
+ */
+
+public class AJLog {
+    public static void log() {
+        System.out.println("===========使用JitPack");
+    }
+}
