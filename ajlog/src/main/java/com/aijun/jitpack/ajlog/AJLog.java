0ec0e1b995364d88c604c70a099f1b081faca8d0
aijunx <aijun198600@126.com>
2017-11-02 00:53:53 +0800

    添加项目之间的依赖

--- ajlog/src/main/java/com/aijun/jitpack/ajlog/AJLog.java
+++ ajlog/src/main/java/com/aijun/jitpack/ajlog/AJLog.java
@@ -4,8 +4,10 @@ package com.aijun.jitpack.ajlog;
  * Created by Administrator on 2017/11/1.
  */
 
+import com.aijun.jitpack.ajotherlog.AJOtherLog;
+
 public class AJLog {
     public static void log() {
-        System.out.println("===========使用JitPack");
+        System.out.println(AJOtherLog.message);
     }
 }
